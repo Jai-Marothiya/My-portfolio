210787dc2e71b6eaa020ded177f26f6d3114cb10
Jai Marothiya <jaimarothiya48@gmail.com>
2022-11-29 10:50:42 +0530

    Creating responsive navbar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      'tablet': '540px',
+      'laptop': '860px',
+      'pc':'1084px',
+      'desktop': '1400px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
